d0104f1c570a24d49d626a1b7818ab0f287a2bc2
Hubert Dang <hubertdang@gmail.com>
2025-11-25 21:44:40 -0500

    Implement password file functions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sysc4810-assignment
 A user authentication and access control system prototype.
+
+## Setup Instructions
+
+Please follow these steps from the project root directory:
+
+### 1. Create and Activate a Virtual Environment
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+### 2. Install Dependencies
+
+```bash
+pip install -r requirements.txt
+```
---
+++ src/problem2c.py
@@ -0,0 +1,59 @@
+from typing import NamedTuple
+from pathlib import Path
+from argon2 import PasswordHasher
+from argon2 import Type as ArgonType
+from argon2.exceptions import HashingError, VerifyMismatchError
+
+PASSWD_FILE = Path(__file__).parent.parent / 'passwd.txt'
+
+RECORD_DELIMITER = ':'
+NUM_RECORD_FIELDS = 2
+
+
+class UserRecord(NamedTuple):
+    uname: str
+    passwd_hash: str
+
+
+TIME_COST = 2  # Number of iterations
+PARALLELISM = 1  # 1 lane
+MEMORY_COST_KIB = 19456  # 19 MiB
+SALT_LENGTH = 16  # 128-bit salt
+HASH_LENGTH = 32  # 256-bit hash
+
+# Argon2id hashing algorithm config (OWASP recommendations)
+ph = PasswordHasher(
+    time_cost=TIME_COST,
+    parallelism=PARALLELISM,
+    memory_cost=MEMORY_COST_KIB,
+    salt_len=SALT_LENGTH,
+    hash_len=HASH_LENGTH,
+    type=ArgonType.ID
+)
+
+
+def add_user_record(uname: str, plaintext_passwd: str) -> None:
+    """
+    Hashes and salts a plaintext password and adds a new user record to the
+    password file.
+    """
+    passwd_hash = ph.hash(plaintext_passwd)  # Argon2id adds salt for you
+    record = [uname, passwd_hash]
+
+    with open(PASSWD_FILE, 'a', encoding='utf-8') as passwd_file:
+        passwd_file.write(RECORD_DELIMITER.join(record) + '\n')
+
+
+def get_user_record(uname: str) -> UserRecord | None:
+    """
+    Retrieves a user record from the password file.
+    """
+    with open(PASSWD_FILE, 'r', encoding='utf-8') as passwd_file:
+        for line in passwd_file:
+            stripped_line = line.rstrip('\n')
+            if stripped_line.startswith(uname + RECORD_DELIMITER):
+                fields = stripped_line.split(RECORD_DELIMITER, maxsplit=1)
+                if len(fields) == NUM_RECORD_FIELDS:
+                    _, passwd_hash = fields
+                    return UserRecord(uname=uname, passwd_hash=passwd_hash)
+    return None
